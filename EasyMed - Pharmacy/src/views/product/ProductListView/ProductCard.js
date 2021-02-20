76b6345edc0f2a3dd78a26cec6769f9898e86451
AdityaTeltia <teltia.aditya22@gmail.com>
2021-02-21 00:16:19 +0530

    Pharmacy Interface

---
+++ EasyMed - Pharmacy/src/views/product/ProductListView/ProductCard.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+  makeStyles
+} from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  statsItem: {
+    alignItems: 'center',
+    display: 'flex'
+  },
+  statsIcon: {
+    marginRight: theme.spacing(1)
+  }
+}));
+
+const ProductCard = ({ className, product, ...rest }) => {
+  const classes = useStyles();
+
+  return (
+    <Card
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <CardContent>
+        <Box
+          display="flex"
+          justifyContent="center"
+          mb={3}
+        >
+          <Avatar
+            alt="Product"
+            src={product.media}
+            variant="square"
+          />
+        </Box>
+        <Typography
+          align="center"
+          color="textPrimary"
+          gutterBottom
+          variant="h4"
+        >
+          {product.Name}
+        </Typography>
+        <Typography
+          align="center"
+          color="textPrimary"
+          variant="body1"
+        >
+          {product.Prescription}
+        </Typography>
+      </CardContent>
+      <Box flexGrow={1} />
+      <Divider />
+      <Box p={2}>
+        <Grid
+          container
+          justify="space-between"
+          spacing={2}
+        >
+          <Grid
+            className={classes.statsItem}
+            item
+          >
+            <AccessTimeIcon
+              className={classes.statsIcon}
+              color="action"
+            />
+            <Typography
+              color="textSecondary"
+              display="inline"
+              variant="body2"
+            >
+              Since:
+              {product.Dated}
+            </Typography>
+          </Grid>
+          <Grid
+            className={classes.statsItem}
+            item
+          >
+            <Typography
+              color="textSecondary"
+              display="inline"
+              variant="body2"
+            >
+              Age:
+              {product.Age}
+              {' '}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </Card>
+  );
+};
+
+ProductCard.propTypes = {
+  className: PropTypes.string,
+  product: PropTypes.object.isRequired
+};
+
+export default ProductCard;
---
+++ EasyMed - Pharmacy/src/views/product/ProductListView/data.js
@@ -0,0 +1,52 @@
+import { v4 as uuid } from 'uuid';
+
+export default [
+  {
+    id: uuid(),
+    Dated: '27/03/2019',
+    Prescription: '2 Dispirin , 1 Paracetamol',
+    media: '/static/images/avatars/avatar_1.png',
+    Name: 'John Doe',
+    Age: '33'
+  },
+  {
+    id: uuid(),
+    Dated: '31/03/2019',
+    Prescription: '2 Dispirin , 1 Paracetamol.',
+    media: '/static/images/avatars/avatar_2.png',
+    Name: 'John Doe',
+    Age: '33'
+  },
+  {
+    id: uuid(),
+    Dated: '03/04/2019',
+    Prescription: '2 Dispirin , 1 Paracetamol',
+    media: '/static/images/avatars/avatar_3.png',
+    Name: 'John Doe',
+    Age: '33'
+  },
+  {
+    id: uuid(),
+    Dated: '04/04/2019',
+    Prescription: '2 Dispirin , 1 Paracetamol',
+    media: '/static/images/avatars/avatar_4.png',
+    Name: 'John Doe',
+    Age: '33'
+  },
+  {
+    id: uuid(),
+    Dated: '04/04/2019',
+    Prescription: '2 Dispirin , 1 Paracetamol',
+    media: '/static/images/avatars/avatar_5.png',
+    Name: 'John Doe',
+    Age: '33'
+  },
+  {
+    id: uuid(),
+    Dated: '04/04/2019',
+    Prescription: '2 Dispirin , 1 Paracetamol',
+    media: '/static/images/avatars/avatar_6.png',
+    Name: 'John Doe',
+    Age: '33'
+  }
+];
